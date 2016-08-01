08cb958207531bd5ad7d6ad93d05ec59a82cadce
Sébastien GUIMMARA <sguimmara@pobox.com>
2016-08-02 00:22:54 +0200

    Cache: Add caching mechanism through CacheManager and the ICacheable interface

---
+++ Assets/Scripts/Geodesy/ICacheable.cs
@@ -0,0 +1,10 @@
+﻿namespace Geodesy
+{
+	/// <summary>
+	/// Provides services to allow caching of an object.
+	/// </summary>
+	public interface ICacheable
+	{
+		ulong InstanceId { get; set; }
+	}
+}
