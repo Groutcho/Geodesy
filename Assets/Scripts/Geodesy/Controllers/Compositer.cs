8d496eacce9e7d4fce15d01bf0aeb73c03ec2a7c
Sébastien GUIMMARA <sguimmara@pobox.com>
2016-08-03 21:20:00 +0200

    Compositing: Add Compositer class

---
+++ Assets/Scripts/Geodesy/Controllers/Compositer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+namespace Geodesy.Controllers
+{
+	/// <summary>
+	/// Class responsible for compositing raster images into the map to be later cut into tiles assigned to the 3D patches on the surface of the globe.
+	/// </summary>
+	public class Compositer : MonoBehaviour
+	{
+		public Camera CompositerCamera;
+
+		// Set to true when a render is necessary.
+		private bool isDirty;
+
+		public void Start ()
+		{
+			isDirty = true;
+		}
+
+		/// <summary>
+		/// Triggers the compositing process.
+		/// </summary>
+		public void Render ()
+		{
+			Debug.Log ("Compositing pass.");
+			if (CompositerCamera == null)
+			{
+				Debug.LogError ("No compositing camera. Aborting rendering.");
+				return;
+			}
+		}
+
+		void LateUpdate ()
+		{
+			if (isDirty)
+			{
+				isDirty = false;
+				Render ();
+			}
+		}
+	}
+}
+
