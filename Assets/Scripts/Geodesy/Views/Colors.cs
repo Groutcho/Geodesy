7ccd29bc2b01bd30fad5cd49c77d9939a629fa75
Sébastien GUIMMARA <sguimmara@pobox.com>
2016-08-19 20:38:37 +0200

    Colors: brighten DarkRed

--- Assets/Scripts/Geodesy/Views/Colors.cs
+++ Assets/Scripts/Geodesy/Views/Colors.cs
@@ -7,7 +7,7 @@ namespace Geodesy.Views
 	{
 		public static readonly Color LightGrey = new Color (0.8f, 0.8f, 0.8f, 0.3f);
 		public static readonly Color DarkGrey = new Color (0.4f, 0.4f, 0.4f, 0.3f);
-		public static readonly Color DarkRed = new Color (0.4f, 0.0f, 0.0f, 0.8f);
+		public static readonly Color DarkRed = new Color (0.8f, 0.0f, 0.0f, 0.9f);
 		public static readonly Color DarkBlue = new Color (0.0f, 0.0f, 0.4f, 0.8f);
 		public static readonly Color Cyan = Color.cyan;
 	}
