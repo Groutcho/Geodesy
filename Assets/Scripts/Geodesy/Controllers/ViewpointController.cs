2347d15b1809c23cb2b711c93952b035ebbef12d
Sébastien GUIMMARA <sguimmara@pobox.com>
2016-08-21 16:08:06 +0200

    ViewpointController: show frustum with gizmos

--- Assets/Scripts/Geodesy/Controllers/ViewpointController.cs
+++ Assets/Scripts/Geodesy/Controllers/ViewpointController.cs
@@ -19,12 +19,14 @@ namespace Geodesy.Controllers
 
 	public delegate void CameraMovedEventHandler (object sender, CameraMovedEventArgs args);
 
-	public class ViewpointController : MonoBehaviour
+	public class ViewpointController : MonoBehaviour, IConsoleCommandHandler
 	{
 		Viewpoint viewpoint;
 		Vector3 lastPos;
 
 		public static ViewpointController Instance { get; private set; }
+
+		public bool ShowFrustum { get; set; }
 
 		public event CameraMovedEventHandler HasMoved;
 
@@ -38,8 +40,10 @@ namespace Geodesy.Controllers
 
 		public void Initialize (Viewpoint viewpoint)
 		{
+			ShowFrustum = true;
 			this.viewpoint = viewpoint;
 			lastPos = transform.position;
+			Views.Debugging.Console.Instance.Register (this, "frustum");
 		}
 
 		public void Update ()
@@ -53,6 +57,68 @@ namespace Geodesy.Controllers
 				}
 			}
 		}
+
+		public void OnDrawGizmos ()
+		{
+			if (ShowFrustum)
+			{
+				Color original = Gizmos.color;
+				Gizmos.color = Color.yellow;
+				Gizmos.matrix = viewpoint.Camera.transform.localToWorldMatrix;
+				Gizmos.DrawFrustum (
+					viewpoint.Camera.transform.position,
+					viewpoint.Camera.fieldOfView,
+					viewpoint.Camera.farClipPlane,
+					viewpoint.Camera.nearClipPlane,
+					viewpoint.Camera.aspect);
+				Gizmos.color = original;
+			}
+		}
+
+		#region IConsoleCommandHandler implementation
+
+		public CommandResult ExecuteCommand (string[] argument)
+		{
+			string keyword = argument [0];
+			switch (keyword)
+			{
+				case "frustum":
+					return ExecuteFrustumCommands (argument);
+				default:
+					throw new NotImplementedException ();
+			}
+		}
+
+		private CommandResult ExecuteFrustumCommands (string[] argument)
+		{
+			if (argument.Length == 1)
+			{
+				return new CommandResult (ShowFrustum);
+			} else if (argument.Length == 2)
+			{
+				bool? show = Views.Debugging.Console.GetThruthValue (argument [1]);
+				if (show.HasValue)
+				{
+					ShowFrustum = show.Value;
+					return new CommandResult (ShowFrustum);
+				} else
+				{
+					throw new ArgumentException ("Expected truth value, got: " + argument [1]);
+				}
+			}
+
+			throw new NotImplementedException ();
+		}
+
+		public string Name
+		{
+			get
+			{
+				return "ViewpointController";
+			}
+		}
+
+		#endregion
 	}
 }
 
