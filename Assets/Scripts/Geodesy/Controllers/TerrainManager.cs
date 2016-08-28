e92e61a7c6042152839f40cba593f11d6dceb3f2
Sébastien GUIMMARA <sguimmara@pobox.com>
2016-08-29 00:31:46 +0200

    TerrainManager: load and sample SRTM data

--- Assets/Scripts/Geodesy/Controllers/TerrainManager.cs
+++ Assets/Scripts/Geodesy/Controllers/TerrainManager.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections;
+using System.IO;
+using Geodesy.Models;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Geodesy.Controllers
 {
 	public class TerrainManager
 	{
 		private static TerrainManager instance;
+
+		IList<SrtmTile> tiles = new List<SrtmTile> (140);
 
 		public static TerrainManager Instance
 		{
@@ -20,12 +26,48 @@ namespace Geodesy.Controllers
 			}
 		}
 
+		public TerrainManager ()
+		{
+			DirectoryInfo di = new DirectoryInfo (@"C:\SRTM\srtm");
+			foreach (var hgt in di.GetFiles())
+			{
+				tiles.Add (Load (new Uri (hgt.FullName)));
+			}
+		}
+
+		static readonly Regex hgtRegex = new Regex (@"(N|S)(\d+)(E|W)(\d+)");
+
+		private SrtmTile Load (Uri uri)
+		{
+			byte[] data = File.ReadAllBytes (uri.AbsolutePath);
+
+			string name = Path.GetFileNameWithoutExtension (uri.AbsolutePath);
+
+			Match m = hgtRegex.Match (name);
+			int lat = int.Parse (m.Groups [2].Value);
+			int lon = int.Parse (m.Groups [4].Value);
+			if (m.Groups [1].Value == "S")
+				lat *= -1;
+			if (m.Groups [3].Value == "W")
+				lon *= -1;
+
+			return new SrtmTile (lat, lon, data);
+		}
+
 		/// <summary>
 		/// Return the elevation of the point at specified coordinates.
 		/// If no elevation data is available, return zero.
 		/// </summary>
 		public float GetElevation (float lat, float lon)
 		{
+			foreach (var item in tiles)
+			{
+				if (item.Contains (lat, lon))
+				{
+					return item.Sample (lat, lon);
+				}
+			}
+
 			return 0f;
 		}
 	}
---
+++ Assets/Scripts/Geodesy/Models/Terrain/SrtmTile.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Geodesy.Models
+{
+	public enum SrtmFormat
+	{
+		SRTM3 = 1201,
+		SRTM1 = 3601
+	}
+
+	public class SrtmTile
+	{
+		private const int NoData = short.MinValue;
+
+		private byte[] data;
+
+		public SrtmFormat Format { get; set; }
+
+		private float latitude;
+		private float longitude;
+
+		public SrtmTile (float lat, float lon, byte[] data)
+		{
+			this.latitude = lat;
+			this.longitude = lon;
+			this.data = data;
+
+			if (data.Length == 1201 * 1201 * 2)
+				Format = SrtmFormat.SRTM3;
+			else if (data.Length == 3601 * 3601 * 2)
+				Format = SrtmFormat.SRTM1;
+			else
+				throw new FormatException ("Invalid SRTM data");
+		}
+
+		public bool Contains (float pLat, float pLon)
+		{
+			return pLat >= latitude &&
+			pLat < latitude + 1 &&
+			pLon >= longitude &&
+			pLon < longitude + 1;
+		}
+
+		/// <summary>
+		/// Return the elevation in meters at the specified point.
+		/// </summary>
+		/// <returns>The elevation.</returns>
+		/// <param name="point">Point.</param>
+		public int Sample (float pLat, float pLon)
+		{
+			double lon = pLon - longitude;
+			double lat = pLat - latitude;
+
+			int size = (int)Format;
+
+			int y = (int)(size - (size * lat));
+			int x = (int)(size * lon);
+
+			int index = sizeof(short) * ((y - 1) * size + (x - 1));
+
+			if (data.Length <= index || index < 0)
+				return 0;
+
+			short msb = (short)data [index];
+			short lsb = (short)data [index + 1];
+
+			short result = (short)(msb << 8 | lsb);
+
+			if (result == NoData)
+			{
+				return 0;
+			}
+
+			// SRTM data is big endian
+			return result;
+		}
+	}
+}
+
