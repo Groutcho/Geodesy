73f2badaac35223773790da7213556ccb9f62e6a
Sébastien GUIMMARA <sguimmara@pobox.com>
2016-08-31 22:01:54 +0200

    Console: add CommandException

---
+++ Assets/Scripts/Geodesy/Views/Debugging/CommandException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Geodesy.Views.Debugging
+{
+	public class CommandException : Exception
+	{
+		public CommandException (string expected, object got) :
+			base (string.Format ("Expected: '{0}', got: '{1}'", expected, got))
+		{
+		}
+
+		public CommandException (string usage) :
+			base (string.Format ("usage: {0}", usage))
+		{
+		}
+	}
+}
+
