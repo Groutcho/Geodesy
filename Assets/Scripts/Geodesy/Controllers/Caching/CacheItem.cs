979b3473ea0f90cad70e0115d03cc5aab5a65f6f
Sébastien GUIMMARA <sguimmara@pobox.com>
2016-09-05 19:34:27 +0200

    CacheItem: implement IEquatable<CacheItem>

--- Assets/Scripts/Geodesy/Controllers/Caching/CacheItem.cs
+++ Assets/Scripts/Geodesy/Controllers/Caching/CacheItem.cs
@@ -2,7 +2,7 @@
 
 namespace Geodesy.Controllers.Caching
 {
-	internal class CacheItem
+	class CacheItem : IEquatable<CacheItem>
 	{
 		public byte[] Data { get; private set; }
 
@@ -17,6 +17,33 @@ namespace Geodesy.Controllers.Caching
 
 			this.Data = data;
 			this.Hash = hash;
+		}
+
+		#region IEquatable implementation
+
+		public bool Equals (CacheItem other)
+		{
+			return other.Hash == this.Hash;
+		}
+
+		#endregion
+
+		public override int GetHashCode ()
+		{
+			return Hash.GetHashCode ();
+		}
+
+		public override string ToString ()
+		{
+			return string.Format ("[CacheItem: Data={0}, Hash={1}]", Data, Hash);
+		}
+
+		public override bool Equals (object obj)
+		{
+			if (obj is CacheItem)
+				return (Equals (obj as CacheItem));
+
+			return false;
 		}
 	}
 }
