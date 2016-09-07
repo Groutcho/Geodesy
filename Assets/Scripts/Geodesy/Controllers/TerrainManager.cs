e491d2a5dd4b05e4ce1947dd1bddc14f0189c500
Sébastien GUIMMARA <sguimmara@pobox.com>
2016-09-07 22:29:33 +0200

    TerrainManager: unload tiles located at the opposite of the current position

--- Assets/Scripts/Geodesy/Controllers/TerrainManager.cs
+++ Assets/Scripts/Geodesy/Controllers/TerrainManager.cs
@@ -49,6 +49,10 @@ namespace Geodesy.Controllers
 		{
 			LatLon point = ViewpointController.Instance.CurrentPosition;
 			RequestTilesAroundPoint (point);
+
+			int oppositeLat = ((int)point.Latitude + 90) % 90;
+			int oppositeLon = ((int)point.Longitude + 180) % 180;
+			RequestUnloadTilesAroundPoint (oppositeLat, oppositeLon);
 		}
 
 		/// <summary>
@@ -72,6 +76,56 @@ namespace Geodesy.Controllers
 			RequestTile (lat + 1, lon + 1);
 		}
 
+		/// <summary>
+		/// Unload the four tiles around the specified point.
+		/// </summary>
+		private void RequestUnloadTilesAroundPoint (int lat, int lon)
+		{
+			UnloadTile (lat, lon);
+			UnloadTile (lat, lon - 1);
+			UnloadTile (lat + 1, lon);
+			UnloadTile (lat + 1, lon + 1);
+		}
+
+		private void UnloadTile (int lat, int lon)
+		{
+			if (lat < 0)
+				lat += 90;
+
+			if (lon < 0)
+				lon += 180;
+
+			if (lat < 0 || lon < 0)
+			{
+				UnityEngine.Debug.LogError (string.Format ("lat: {0}, lon: {1}", lat, lon));
+				return;
+			}
+
+			// Remove the tile from the requests
+			if (gridStatus [lon, lat] == (byte)TileStatus.Requested)
+			{
+				Uri toRemove = null;
+				foreach (var request in pendingRequests)
+				{
+					if ((int)request.Value.Latitude == lat && (int)request.Value.Longitude == lon)
+					{
+						toRemove = request.Key;
+						break;
+					}
+				}
+
+				if (toRemove != null)
+					pendingRequests.Remove (toRemove);
+			}
+
+			grid [lon, lat] = null;
+		}
+
+		/// <summary>
+		/// Request the download of the tile at the specified coordinates.
+		/// </summary>
+		/// <param name="lat">Lat.</param>
+		/// <param name="lon">Lon.</param>
 		private void RequestTile (int lat, int lon)
 		{
 			// Tile already requested
@@ -81,9 +135,12 @@ namespace Geodesy.Controllers
 			char northing = lat < 0 ? 'S' : 'N';
 			char easting = lon < 0 ? 'W' : 'E';
 			string name = string.Format ("{0}{1:00}{2}{3:000}.hgt", northing, Math.Abs (lat), easting, Math.Abs (lon));
+
 			LatLon position = new LatLon (lat, lon);
+
 			Uri request = new Uri (terrainProvider, name);
 			pendingRequests.Add (request, position);
+
 			gridStatus [lon + 180, lat + 90] = (byte)TileStatus.Requested;
 
 			Cache.Instance.Get (request, OnDownloadCompleted);
@@ -93,13 +150,15 @@ namespace Geodesy.Controllers
 		{
 			if (!pendingRequests.ContainsKey (uri))
 			{
-				// TODO: Log error
+				// The request has been cancelled, nothing to do.
 				return;
 			}
 
 			SrtmTile tile = new SrtmTile (pendingRequests [uri], data);
 			grid [tile.Easting, tile.Northing] = tile;
 			gridStatus [tile.Easting, tile.Northing] = (byte)TileStatus.Loaded;
+
+			pendingRequests.Remove (uri);
 		}
 
 		/// <summary>
