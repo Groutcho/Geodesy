9b39ad61f678c68de13eecc23f6c9974e80385a7
Sébastien GUIMMARA <sguimmara@pobox.com>
2016-09-08 23:46:47 +0200

    PatchRequest: implement IEquatable<PatchRequest>

--- Assets/Scripts/Geodesy/Controllers/Workers/PatchRequest.cs
+++ Assets/Scripts/Geodesy/Controllers/Workers/PatchRequest.cs
@@ -1,8 +1,9 @@
-﻿using Geodesy.Models;
+﻿using System;
+using Geodesy.Models;
 
 namespace Geodesy.Controllers.Workers
 {
-	public class PatchRequest
+	public class PatchRequest : IEquatable<PatchRequest>
 	{
 		public readonly Location Location;
 		public readonly int Subdivisions;
@@ -13,6 +14,38 @@ namespace Geodesy.Controllers.Workers
 		{
 			this.Location = location;
 			this.Subdivisions = subdivisions;
+		}
+
+		public bool Equals(PatchRequest other)
+		{
+			if (other.Location == Location && other.Subdivisions == Subdivisions)
+				return true;
+
+			return false;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj is PatchRequest)
+				return Equals(obj as PatchRequest);
+
+			return false;
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				int hash = 17;
+				hash = hash * 23 + Location.GetHashCode();
+				hash = hash * 23 + Subdivisions;
+				return hash;
+			}
+		}
+
+		public override string ToString()
+		{
+			return string.Format("[PatchRequest {0} {1} subdivisions", Location, Subdivisions);
 		}
 	}
 }
