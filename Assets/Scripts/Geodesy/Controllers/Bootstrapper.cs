2b2c285f8f30dcf427323939d5b3557a8a13a5b3
Sébastien GUIMMARA <sguimmara@pobox.com>
2016-09-10 14:13:53 +0200

    Architecture: Bootstrapper class initializes the MainController

---
+++ Assets/Scripts/Geodesy/Controllers/Bootstrapper.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace OpenTerra.Controllers
+{
+	public class Bootstrapper : MonoBehaviour
+	{
+		public Gradient ElevationColorRamp;
+		public int[] ScenesToLoad;
+
+		private bool ready;
+
+		private MainController mainController;
+
+		public void Start()
+		{
+			StartCoroutine(Startup());
+		}
+
+		private IEnumerator Startup()
+		{
+			foreach (var scene in ScenesToLoad)
+			{
+				SceneManager.LoadScene(scene, LoadSceneMode.Additive);
+				yield return new WaitForEndOfFrame();
+			}
+
+			mainController = new MainController(ElevationColorRamp);
+
+			ready = true;
+		}
+
+		public void Update()
+		{
+			if (!ready)
+				return;
+
+			mainController.Update();
+		}
+
+		public void OnDrawGizmos()
+		{
+			if (!ready)
+				return;
+
+			mainController.OnDrawGizmos();
+		}
+	}
+}
