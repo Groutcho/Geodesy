4aa005ddcdfe0eff226ecfb2d2437b9523430e5f
Sébastien GUIMMARA <sguimmara@pobox.com>
2016-09-10 13:28:33 +0200

    Datum: implement ray/ellipsoid intersection

--- Assets/Scripts/Geodesy/Models/Datum.cs
+++ Assets/Scripts/Geodesy/Models/Datum.cs
@@ -54,5 +54,45 @@ namespace OpenTerra.Models
 			m = Transform * m;
 			return new Circle (radius, m);
 		}
+
+		/// <summary>
+		/// Test intersection between the ellipsoid and the provided ray.
+		/// </summary>
+		/// <param name="origin">The origin of the ray.</param>
+		/// <param name="direction">The direction of the ray.</param>
+		/// <param name="intersection">If an intersection occurs, represents the point of intersection.</param>
+		/// <returns>True if the ray intersects with the ellipsoid.</returns>
+		public bool Intersects(Cartesian3 origin, Cartesian3 direction, out Cartesian3 intersection)
+		{
+			double a = SemimajorAxis;
+			double b = SemiminorAxis;
+
+			double x = origin.x;
+			double y = origin.y;
+			double z = origin.z;
+
+			double u = direction.x;
+			double v = direction.y;
+			double w = direction.z;
+
+			double k = (b * b * (u * x + v * y) + a * a * w * z);
+
+			double t = -(1 / (b * b * (u * u + v * v) + a * a * w * w))
+				* (b * b * (u * x + v * y) + a * a * w * z + 0.5
+				* Math.Sqrt(
+					4 * k * k
+					- 4 * (b * b * (u * u + v * v) + a * a * w * w)
+					* (b * b * (-a * a + x * x + y * y) + a * a * z * z))
+					);
+
+			if (double.IsNaN(t))
+			{
+				intersection = Cartesian3.Zero;
+				return false;
+			}
+
+			intersection = origin + t * direction;
+			return true;
+		}
 	}
 }
