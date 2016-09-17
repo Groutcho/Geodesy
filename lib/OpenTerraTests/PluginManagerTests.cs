7234e12362424983d037657cc688ac009b8e55a1
Sébastien GUIMMARA <sguimmara@pobox.com>
2016-09-17 12:33:44 +0200

    Tests: add PluginManager tests

---
+++ lib/OpenTerraTests/PluginManagerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenTerra.Plugins;
+
+namespace OpenTerraTests
+{
+    [TestClass]
+    public class PluginManagerTests
+    {
+        [TestMethod]
+        public void Constructor_load_test_plugin()
+        {
+            PluginManager manager = new PluginManager(@"C:\tmp");
+            Assert.AreEqual(1, manager.PluginCount);
+
+            Assert.AreEqual("KML-import", manager.LoadedPlugins[0].Name);
+        }
+    }
+}
