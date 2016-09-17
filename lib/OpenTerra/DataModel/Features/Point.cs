a5c22991d1af2b8e767694c88f2fb803c65c78bc
Sébastien GUIMMARA <sguimmara@pobox.com>
2016-09-17 18:27:00 +0200

    Features: add Geometry & Point classes

---
+++ lib/OpenTerra/DataModel/Features/Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenTerra.DataModel.Features
+{
+	public class Point : Geometry
+	{
+		public LatLon Coordinates { get; set; }
+
+		public Point(LatLon coordinates)
+		{
+			this.Coordinates = coordinates;
+		}
+	}
+}
